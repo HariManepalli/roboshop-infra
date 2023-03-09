0f11c8f464c1b4e217d92b8516a666b67331eb8e
Hariprasad <hari20185@gmail.com>
2023-03-09 11:13:18 +0530

    roboshop infra changes

--- ec2/main.tf
+++ ec2/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "sg1" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z103214126L48SQW30RSR"
-  name    = "${var.component}-dev.devopsb71.online"
+  name    = "${var.component}-dev.devopsb71m.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
