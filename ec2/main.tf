1a068015a10748a1b581a30e2c524d63cdf9ed65
Hariprasad <hari20185@gmail.com>
2023-03-09 11:08:32 +0530

    roboshop infra changes

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "ec2" {
   iam_instance_profile   = "${var.env}-${var.component}-role"
   tags = {
     Name    = var.component
-    Monitor = var.monitor ? "yes" : "no"
   }
 }
 
