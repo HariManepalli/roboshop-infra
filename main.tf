14cfd4bfdddda22ff23fbfb50ce2c1448c22fe74
Hariprasad <hari20185@gmail.com>
2023-03-25 20:51:51 +0530

    roboshop infra changes

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
 
   source = "git::https://github.com/HariManepalli/tf-module-vpc.git"
   env = var.env
+  tags = var.tags
 
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
