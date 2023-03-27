e7def2332256a361b8903faddd66a2430c7a0376
Hariprasad <hari20185@gmail.com>
2023-03-27 09:57:49 +0530

    roboshop infra changes

--- main.tf
+++ main.tf
@@ -10,3 +10,22 @@ module "vpc" {
   public_subnets  = each.value["public_subnets"]
   private_subnets = each.value["private_subnets"]
 }
+
+module "docdb" {
+  source              = "git::https://github.com/HariManepalli/tf-module-docdb.git"
+  env                 = var.env
+  tags                = var.tags
+
+  subnet_ids                       = local.db_subnets_ids
+
+  for_each        = var.docdb
+  engine          = each.value["engine"]
+  backup_retention_period          = each.value["backup_retention_period"]
+  preferred_backup_window          = each.value["preferred_backup_window"]
+  skip_final_snapshot              = each.value["skip_final_snapshot"]
+  engine_version                   = each.value["engine_version"]
+  no_of_instances                  = each.value["no_of_instances"]
+  instance_class                   = each.value["instance_class"]
+}
+
+
--- vars.tf
+++ vars.tf
@@ -3,4 +3,5 @@ variable "tags" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_route_table" {}
+variable "docdb" {}
 
