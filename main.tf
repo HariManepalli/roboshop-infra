b8db4f1c75832f1b039d15609b780d8173e008a8
Hariprasad <hari20185@gmail.com>
2023-03-30 08:47:07 +0530

    roboshop infra changes

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ module "app" {
   env    = var.env
   tags   = var.tags
 
-  for_each           = var.apps
+  for_each           = var.app
   component          = each.value["component"]
   instance_type      = each.value["instance-type"]
 }
