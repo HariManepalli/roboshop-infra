3b359f535f0e13384b43c556c2275ccc6d1bad9d
Hariprasad <hari20185@gmail.com>
2023-04-07 12:35:37 +0530

    roboshop infra changes

--- main.tf
+++ main.tf
@@ -12,22 +12,22 @@ module "vpc" {
 }
 
 module "docdb" {
-  source              = "git::https://github.com/HariManepalli/tf-module-docdb.git"
-  env                 = var.env
-  tags                = var.tags
+  source = "git::https://github.com/raghudevopsb71/tf-module-docdb.git"
+  env    = var.env
+  tags   = var.tags
 
-  subnet_ids          = local.db_subnet_ids
-  vpc_id              = module.vpc["main"].vpc_id
+  subnet_ids = local.db_subnet_ids
+  vpc_id     = module.vpc["main"].vpc_id
 
-  for_each                         = var.docdb
-  engine                           = each.value["engine"]
-  backup_retention_period          = each.value["backup_retention_period"]
-  preferred_backup_window          = each.value["preferred_backup_window"]
-  skip_final_snapshot              = each.value["skip_final_snapshot"]
-  engine_version                   = each.value["engine_version"]
-  no_of_instances                  = each.value["no_of_instances"]
-  instance_class                   = each.value["instance_class"]
-  allow_subnets                    = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+  for_each                = var.docdb
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  no_of_instances         = each.value["no_of_instances"]
+  instance_class          = each.value["instance_class"]
+  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
 }
 
 module "rds" {
